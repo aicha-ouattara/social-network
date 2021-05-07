ca0ef5d34d21239f34759d77124fb32b0fa6bd5c
RV <peterherve88@gmail.com>
2021-05-07 14:16:54 +0200

    class JsonResponse Ok

--- classes/JsonResponse.php
+++ classes/JsonResponse.php
@@ -5,8 +5,6 @@
  */
 class JsonResponse
 {
-	private $response = [];
-
 	function __construct($code, $message= null)
 	{
 		// clear the old headers
@@ -19,24 +17,28 @@ class JsonResponse
 		header('Content-Type: application/json');
 		$status = array(
 			200 => 'OK',
-			201 => 'Created'						//	Requête traitée avec succès et création d’un document.
-			202 => 'Accepted'						//	Requête traitée, mais sans garantie de résultat.
+			201 => 'Created',						//	Requête traitée avec succès et création d’un document.
+			202 => 'Accepted',						//	Requête traitée, mais sans garantie de résultat.
+
 			400 => 'Bad Request', 					//La syntaxe de la requête est erronée.
 			401 => 'Unauthorized',					//Une authentification est nécessaire pour accéder à la ressource.
 			403 => 'Forbidden',						//l'authentification a été acceptée mais les droits d'accès ne permettent pas au client d'accéder à la ressource.
 			404 => 'Not Found',						//Ressource non trouvée.
 			405 => 'Method Not Allowed',			//Méthode de requête non autorisée.
-			409 => 'Conflict'						//	La requête ne peut être traitée en l’état actuel. (user déjà présent en bd)
+			406 => '406 Not Acceptable',					//La ressource demandée n'est pas disponible dans un format qui respecterait les en-têtes « Accept » de la requête.
+			409 => 'Conflict',						//	La requête ne peut être traitée en l’état actuel. (user déjà présent en bd)
 
-			500 => '500 Internal Server Error'		//Erreur interne du serveur.
-			501 => 'Not Implemented'				//Fonctionnalité réclamée non supportée par le serveur.
+			500 => '500 Internal Server Error',		//Erreur interne du serveur.
+			501 => 'Not Implemented',				//Fonctionnalité réclamée non supportée par le serveur.
 		);
 		// ok, validation error, or failure
 		header('Status: '.$status[$code]);
+
 		// return the encoded json
-		return json_encode(array(
+		echo json_encode(array(
 			'status' => $code < 300, // success or not?
-			'message' => $message
+			'message' => $message,
 		));
+
 	}
 }
--- controller/addPost.php
+++ controller/addPost.php
@@ -5,7 +5,7 @@
  */
 class addPost
 {
-	$this->response = [];
+
 
 	function __construct()
 	{
@@ -13,26 +13,38 @@ class addPost
 		// var_dump($_FILES);
 
 		// $target_dir = UPLOADS;
-		$target_file = UPLOADS . basename($_FILES["fileToUpload"]["name"]);
+		$target_file = UPLOADS . basename($_FILES["imageToUpload"]["name"]);
 
 		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 		// Check if image file is a actual image or fake image
 		if(isset($_POST["submit"])) {
-
+			if (!$this->checkSource()) {
+				new JsonResponse(406, "La source n'est pas du bon format");
+				die;
+			}
+			elseif (!$this->checkChoice($_POST["firstAnswer"]) || !$this->checkChoice($_POST["secondAnswer"])) {
+				// code...
+			}
 		}
 		else {
-			$this->response[] = {'error' => }
+			echo new JsonResponse(400, "Aucunes données envoyées");
+			die;
 		}
 	}
 
 	private function checkSource()
 	{
-		$check = getimagesize($_FILES["imageToUpload"]["tmp_name"]);
-		if($check !== false) {
-			echo "File is an image - " . $check["mime"] . ".";
-			return true;
-		} else {
-			echo "File is not an image.";
+		if (isset($_FILES["imageToUpload"]["tmp_name"]) && $_FILES["imageToUpload"]["tmp_name"] != '') {
+			$check = getimagesize($_FILES["imageToUpload"]["tmp_name"]);
+			if($check !== false) {
+				// echo "File is an image - " . $check["mime"] . ".";
+				return true;
+			} else {
+				// echo "File is not an image.";
+				return false;
+			}
+		}
+		else {
 			return false;
 		}
 	}
