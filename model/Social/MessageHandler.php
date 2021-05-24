16799d1adc0d88d509d38eb9e0fd4ba0c89ee49a
Theo <theo.petropoulos@laplateforme.io>
2021-05-24 20:53:45 +0200

    message : emoji & delete ok

--- model/Social/MessageHandler.php
+++ model/Social/MessageHandler.php
@@ -3,14 +3,40 @@
     require '../user/User.php';
     require 'Chat.php';
 
-    class MessageHandler extends Chat{
+    class Message extends Chat{
         private $id = null;
         private $id_conversation = null;
+        private $id_user = null;
 
-        public function __construct(int $id_message, int $id_conversation){
+        public function __construct(int $id_message, int $id_conversation, int $user){
             parent::__construct($id_conversation);
             $this->id = $id_message;
             $this->id_conversation = $id_conversation;
+            $this->user = $user;
+        }
+
+        public function verifyReceiver(){
+            $stmt = self::$db->prepare(
+                'SELECT `id_receiver`
+                FROM `messages` 
+                WHERE `id` = ?'
+            );
+            $stmt->execute([$this->id]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if($result['id_receiver'] == $this->user) return 1;
+            else return 0;
+        }
+
+        public function verifySender(){
+            $stmt = self::$db->prepare(
+                'SELECT `id_sender`
+                FROM `messages` 
+                WHERE `id` = ?'
+            );
+            $stmt->execute([$this->id]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if($result['id_sender'] == $this->user) return 1;
+            else return 0;
         }
 
         public function updateEmoji(int $emoji){
@@ -21,9 +47,23 @@
             );
             $stmt->execute([$emoji, $this->id, $this->id_conversation]);
         }
+
+        public function deleteSelf(){
+            $stmt = self::$db->prepare(
+                'DELETE FROM `messages` 
+                WHERE `id` = ?'
+            );
+            $stmt->execute([$this->id]);
+        }
     }
 
-    if(isset($_POST['emoji']) && $_POST['emoji'] && isset($_POST['message']) && $_POST['message']){
-        $message = new MessageHandler($_POST['message'], $_POST['conversation']);
-        $message->updateEmoji($_POST['emoji']);
+    if(isset($_POST['emoji']) && $_POST['emoji'] && isset($_POST['message']) && $_POST['message'] && isset($_POST['user']) && $_POST['user']){
+        $id_message = intval(str_replace('message_', '', $_POST['message']));
+        $message = new Message($id_message, $_POST['conversation'], $_POST['user']);
+        if($message->verifyReceiver()) $message->updateEmoji($_POST['emoji']);
+    }
+
+    else if(isset($_POST['delete']) && $_POST['delete'] == 1 && isset($_POST['id']) && $_POST['id'] && isset($_POST['user']) && $_POST['user']){
+        $message = new Message(intval($_POST['id']), intval($_POST['conversation']), intval($_POST['user']));
+        if($message->verifySender()) $message->deleteSelf();
     }
--- view/user/conversation.php
+++ view/user/conversation.php
@@ -1,17 +1,31 @@
-<section>
+<?php
+    /**
+     * Define emojis
+     */
+    $emoji_0 = null;
+    $emoji_1 = '<i class="far fa-grin-alt"></i>';
+    $emoji_2 = '<i class="far fa-grin-squint"></i>';
+    $emoji_3 = '<i class="far fa-grin-hearts"></i>';
+    $emoji_4 = '<i class="far fa-grin-beam-sweat"></i>';
+    $emoji_5 = '<i class="far fa-sad-tear"></i>';
+?>
+
+<section id="section_messages">
 <?php foreach($messages as $key => $message){?>
-    <div id="message_<?=$message['id'];?>" class="<?=$message['id_receiver']==$user->getHis('id') ? 'received' : 'sent'?>">
+    <div id="message_<?=$message['id'];?>" class="message_div <?=$message['id_receiver']==$user->getHis('id') ? 'received' : 'sent'?>">
         <p style="display:flex;">
             <?= $message['sender']==null ? 'Utilisateur supprimé' : 
             '<a href="profil&user=' . $message['sender'] . '">' . $message['sender'] . '</a>'; 
             echo ' : ' . $message['content'];?></p>
+            <div class="emoji_div" id='emoji_div_<?=$message['id'];?>'><?=${'emoji_' . $message['emoji']};?></div>
+            <button class="delete_message" id="delete_message_<?=$message['id'];?>">Supprimer</button>
     </div>
-<?php } ?>
-<form action="" id="messager">
-    <input type="text" name="message" id="input_m" autocomplete="off">
-    <button id="submit_m">Envoyer</button>
-</form>
-<a href="messages">Retour aux messages</a>
+    <?php } ?>
+    <form action="" id="messager">
+        <input type="text" name="message" id="input_m" autocomplete="off">
+        <button id="submit_m">Envoyer</button>
+    </form>
+    <a href="messages">Retour aux messages</a>  
 </section>
 
 <style>
@@ -37,23 +51,40 @@
                 )
             }
             else{
-                sendEmoji($(this).attr('class'))
+                sendEmoji($(this).parents('.message_div').attr('id'), $(this).attr('class'), $(this).parents('.message_div').children('.emoji_div').attr('id'))
                 closeEmoji(div_emoji)
             }
         })
 
         $(document).on('click', 'button.emoji_close', function(){ closeEmoji($(this).parent())})
+
+        $(document).on('click', '.delete_message', function(){
+            deleteMessage($(this).parents('.message_div').attr('id'))
+        })
     })
 
     function closeEmoji(div){
         div.removeClass('opened').find('.emoji_1').nextAll().remove()
     }
 
-    function sendEmoji(emoji){
+    function sendEmoji(message, emoji, div){
         emoji = emoji.split(' ')
         emoji = emoji[1].replace('emoji_', '')
-        $.post('model/social/MessageHandler.php', {emoji, message:"<?=$message['id'];?>", conversation:"<?=$message['conversation'];?>"}, function(data){
-            
+        $.post(
+            'model/social/MessageHandler.php', 
+            {emoji, message:message, user:"<?=$user->getHis('id');?>", conversation:"<?=$message['conversation'];?>"}, 
+            function(data){
+                $("#" + div).load(location.href + " #" + div)
+        })
+    }
+
+    function deleteMessage(id){
+        id = id.replace('message_', '')
+        $.post(
+            'model/social/MessageHandler.php', 
+            {delete:1, id, user:"<?=$user->getHis('id');?>", conversation:"<?=$message['conversation'];?>"}, 
+            function(data){
+                $('#section_messages').load(location.href + ' #section_messages')
         })
     }
 </script>
